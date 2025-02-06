1970c602362a032473457a046f51d1d32ab56b03
NaziyaAbdul <110959312+NaziyaAbdul@users.noreply.github.com>
2025-02-06 21:48:37 +0530

    Update 12.sales_by_month.sql

--- SQL Scripts/12.sales_by_month.sql
+++ SQL Scripts/12.sales_by_month.sql
@@ -6,10 +6,4 @@ FROM sales_data
 GROUP BY YEAR(TransactionDate), MONTH(TransactionDate)
 ORDER BY Year DESC, Month DESC;
 
---sales by year
-SELECT 
-  YEAR(TransactionDate) AS Year, 
-  FORMAT(CAST(SUM(TransactionAmount) AS DECIMAL(18,2)), 'N2') AS TotalSales
-FROM sales_data
-GROUP BY YEAR(TransactionDate)
-ORDER BY Year DESC;
+
